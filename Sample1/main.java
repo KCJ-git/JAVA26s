4b6f53bf2c6d99387571c761e4d2cacc23000f74
KCJ-git <ycjkoo@suwon.ac.kr>
2026-03-15 22:55:15 +0900

    Implement basic library management system in Java

---
+++ Sample1/main.java
@@ -0,0 +1,47 @@
+package library;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Library lib = new Library(3);
+		
+		lib.addBook(new Book("자바", "이이이"));
+		lib.addBook(new Book("바바", "이이"));
+		lib.addBook(new Book("자바바", "이이이이"));
+		
+		Scanner sc= new Scanner(System.in);
+		
+		
+	String msg = """
+				1.책 목록보기
+				2.대여하기
+				3.반납하기
+				입력>>""";
+	
+	
+	for(int i=0;i<10;i++) {
+		System.out.println("=================");
+		System.out.print(msg);
+		int choice =sc.nextInt();
+		
+		if(choice==1) {
+			lib.displayInfo();
+		}else if(choice==2) {
+			System.out.print("대여할 책 제목 입력>>");
+			String brbook=sc.next();
+			lib.borrowBook(brbook);
+		
+		
+		}else if(choice==3) {
+			System.out.print("반납할 책 제목 입력>>");
+			String rtbook=sc.next();
+			lib.returnBooks(rtbook);
+		}else {
+			System.out.println("잘못입력하셨습니다");
+			
+		}
+	}return;
+	
+	}
+}
